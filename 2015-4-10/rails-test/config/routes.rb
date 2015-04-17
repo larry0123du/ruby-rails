ec1f4e9fe155dec7ba6de05f45d8fb436b08e183
刘当一 <LeedyPKU@gmail.com>
2015-04-17 11:40:01 +0800

    devise

--- 2015-4-10/rails-test/config/routes.rb
+++ 2015-4-10/rails-test/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :posts do
     collection  { get   "hottest" }
     member      { post  "reply"   }
@@ -7,7 +8,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'posts#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
